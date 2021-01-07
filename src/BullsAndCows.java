7249b55ed92a1d063f432ae7be21c4b17cb52b1f
Ramadhani Mdee <42729286+ramsony@users.noreply.github.com>
2021-01-06 19:45:59 -0500

    The grader

--- src/BullsAndCows.java
+++ src/BullsAndCows.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class BullsAndCows {
-
     public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        // String val = ""+((int)(Math.random()*9000)+1000);
+        String val = "9305";
+        int cow = 0;
+        int bull = 0;
 
+        char[] digit = sc.nextLine().toCharArray();
+        char[] cha1 = val.toCharArray();
+            for(int i = 0; i < digit.length; i++) {
+                if (val.contains(Character.toString(digit[i]))) {
+                    cow++;
+                }
+                if (digit[i] == cha1[i]) {
+                    bull++;
+                    cow--;
+                }
+            }
+            if (bull > 0 && cow == 0) {
+                System.out.println("Grade: " + bull + " bull(s)." + "The secret code is " + val);
+            } else if (cow > 0 && bull == 0) {
+                System.out.println("Grade: " + cow + " cow(s)." + "The secret code is " + val);
+            } else if (bull > 0 && cow > 0) {
+                System.out.println("Grade: " + bull + " bull(s). " + "and " + cow + " cow(s)" + " The secret code is " + val);
+            } else {
+                System.out.println("Grade: " + "None. The secret code is " + val);
+            }
     }
 }
+
