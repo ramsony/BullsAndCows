e3c7f843bb7b2005f511a8f0b78bf96e9ba3c6cb
Ramadhani Mdee <42729286+ramsony@users.noreply.github.com>
2021-01-19 10:40:05 -0500

    1.Ask for the length of the secret code and then generate the code. 2.Wait for the user input. 3.Grade the guessing attempt in bulls and cows. 4.If the secret code has been guessed, the program stops; otherwise, return to the second step.

--- src/BullsAndCows.java
+++ src/BullsAndCows.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
 
 public class BullsAndCows {
-    public static void main(String[]args) {
-        Scanner sc = new Scanner(System.in);
-        // String val = ""+((int)(Math.random()*9000)+1000);
-        String val = "9305";
+    public static String generateRandomNum(int num) {
+        StringBuilder rand = new StringBuilder();
+        while (rand.length() != num) {
+            long pseudoRandomNumber = System.nanoTime();
+            String num1 = String.valueOf(Long.parseLong(new StringBuilder(String.valueOf(pseudoRandomNumber)).reverse().toString()));
+            for (int i = 0; i < num1.length(); i++) {
+                if (!rand.toString().contains(String.valueOf(num1.charAt(i)))) {
+                    rand.append(num1.charAt(i));
+                }
+                if (rand.length() == num) {
+                    break;
+                }
+            }
+        }
+        return rand.toString();
+    }
+    private static String bullsCowCheck(String guess, String val) {
         int cow = 0;
         int bull = 0;
-
-        char[] digit = sc.nextLine().toCharArray();
+        char[] digit = guess.toCharArray();
         char[] cha1 = val.toCharArray();
             for(int i = 0; i < digit.length; i++) {
                 if (val.contains(Character.toString(digit[i]))) {
@@ -19,15 +31,48 @@ public class BullsAndCows {
                     cow--;
                 }
             }
-            if (bull > 0 && cow == 0) {
-                System.out.println("Grade: " + bull + " bull(s)." + "The secret code is " + val);
-            } else if (cow > 0 && bull == 0) {
-                System.out.println("Grade: " + cow + " cow(s)." + "The secret code is " + val);
-            } else if (bull > 0 && cow > 0) {
-                System.out.println("Grade: " + bull + " bull(s). " + "and " + cow + " cow(s)" + " The secret code is " + val);
-            } else {
-                System.out.println("Grade: " + "None. The secret code is " + val);
+        String bulls = bull > 1 ? " bulls." : " bull.";
+        String cows = cow > 1 ? " cows." : " cow.";
+        if (bull > 0 && cow == 0) {
+            return  "Grade: " + bull + bulls ;
+        } else if (cow > 0 && bull == 0) {
+            return "Grade: " + cow + cows ;
+        } else if (bull > 0 && cow > 0) {
+            return "Grade: " + bull + bulls + "and " + cow + cows ;
+        } else {
+            return "Grade: " + "None";
+        }
+    }
+
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please, enter the secret code's length:");
+        int turn = 1;
+        int num = sc.nextInt();
+        while (num > 10) {
+            System.out.println("Error: can't generate a secret number with a length of " +
+                    "" + num + " because there aren't enough unique digits.");
+            System.out.println("Please, enter the secret code's length:");
+            num = sc.nextInt();
+        }
+        String val = generateRandomNum(num);
+        System.out.println("Okay, let's start a game!");
+        System.out.println("Turn "+ turn+":");
+
+        while(true) {
+            String guess = sc.nextLine();
+            while (guess.isEmpty()) {
+                guess = sc.nextLine();
             }
+            if (val.equals(guess)) {
+                String bulls = val.length() > 1 ? " bulls" : " bull";
+                System.out.println("Grade: "+val.length() + bulls+"\nCongratulations! You guessed the secret code.");
+                return;
+            }
+            String result = bullsCowCheck(guess, val);
+            System.out.println(result);
+            turn++;
+            System.out.println("Turn "+ turn+":");
+        }
     }
 }
-
